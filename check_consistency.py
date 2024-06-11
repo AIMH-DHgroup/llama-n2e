a31d5ad027d2f4f3c65823346736793d209fec5c
c.demartino <c.demartino1@studenti.unipi.it>
2024-06-11 12:53:14 +0200

    Update check_consistency.py
    Add model printing

--- check_consistency.py
+++ check_consistency.py
@@ -2,37 +2,33 @@ import json
 
 if __name__ == '__main__':
 
-    def check_broken_sentences(text):
+    def check_broken_sentences(text, mod):
         global all_sentences
         lines = text.split('\n')
-        count_broken_sentences = 0
         for line in lines:
             if line:
                 if not line.endswith(('.', '!', '?')):
-                    count_broken_sentences += 1
                     all_sentences.append(line)
-        return count_broken_sentences
+                    models.append(mod)
 
     # insert filename here
-    input_filename = "narratives_dict_output-all-lastnewprompt-J98.json"
+    input_filename = "narratives_dict_output-write-long-narra.json"
 
     with open(input_filename, 'r', encoding='utf-8') as file:
         input_json = json.load(file)
 
     count_output = {}
-    count_broken = {}
     all_sentences = []
+    models = []
     for model in input_json.keys():
         count_out = 0
         for narrative in input_json[model]:
             for prompt in input_json[model][narrative]:
                 for output in input_json[model][narrative][prompt]:
-                    broken_sentences = check_broken_sentences(input_json[model][narrative][prompt][output])
+                    check_broken_sentences(input_json[model][narrative][prompt][output], model)
                     count_out += 1
-                    count_broken[model] = broken_sentences
         count_output[model] = count_out
-    print("Broken sentences: ", count_broken, "\n")
     print("Total saved outputs: ", count_output, "\n")
     print("Printing all sentences...\n")
-    for elem in all_sentences:
-        print(elem)
+    for model, elem in zip(models, all_sentences):
+        print(model, " --> ", elem)
